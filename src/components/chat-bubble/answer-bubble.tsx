394fbca66389cd15902080da9c22791813783687
LZH <LZH01101110@163.com>
2025-06-13 17:45:30 +0800

    feat: 设置md样式

--- src/components/chat-bubble/answer-bubble.tsx
+++ src/components/chat-bubble/answer-bubble.tsx
@@ -1,10 +1,23 @@
-import MarkdownIt from "markdown-it";
+import markdownIt from "markdown-it";
 import type {Component} from "solid-js";
+import hljs from "highlight.js";
+import 'highlight.js/styles/atom-one-light.css';
+import type MarkdownIt from "markdown-it/index.js"; // 也可以用 'atom-one-dark.css' 等
 
-const md = MarkdownIt({
+const md:MarkdownIt = markdownIt({
     html: true,
     linkify: true,
     typographer: true,
+    breaks:true,
+    highlight(code, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+            try {
+                return `<pre class="hljs p-2 bg-[#f9f9f9] rounded-lg border-solid border-1 border-[#e5e5e5]"><code>${hljs.highlight(code, {language: lang}).value}</code></pre>`;
+            } catch (_) {
+            }
+        }
+        return `<pre class="hljs p-2 bg-[#f9f9f9] rounded-lg border-solid border-1 border-[#e5e5e5]"><code>${md.utils.escapeHtml(code)}</code></pre>`;
+    },
 });
 
 type AnswerBubbleProps = {
@@ -13,14 +26,13 @@ type AnswerBubbleProps = {
 
 const AnswerBubble: Component<AnswerBubbleProps> = (props) => {
     const html = () => md.render(props.message);
+    console.log(html())
     return (
-        <div class="chat chat-start">
-            <div class="chat-bubble">
-                <div
-                    class="markdown-body"
-                    innerHTML={html()}
-                />
-            </div>
+        <div class="card p-4 mb-2 mt-4">
+            <div
+                class="markdown-body prose prose-base leading-loose"
+                innerHTML={html()}
+            />
         </div>
     );
 };

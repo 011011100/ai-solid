59244c01943927bc3d7fb4878196939ebc842867
LZH <LZH01101110@163.com>
2025-06-17 15:24:34 +0800

    feat: 抽出api

---
+++ src/api/api.ts
@@ -0,0 +1,15 @@
+const BASE_URL = 'http://localhost:8080/'
+
+const askQuestionApi = ((question: string , conversationId: string): string => {
+    return `${BASE_URL}askQuestion?question=${question}&conversationId=${conversationId}`;
+})
+
+
+const getHistoryMessageApi = ((conversationId: string): string => {
+    return `${BASE_URL}getHistoryMessage?conversationId=${conversationId}`;
+})
+
+export {
+    askQuestionApi,
+    getHistoryMessageApi,
+};

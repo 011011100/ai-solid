a4e2bd1c7f60d0e3f56d47e4226bfafed3d9bc68
LZH <LZH01101110@163.com>
2025-06-17 17:16:25 +0800

    feat:添加侧栏

--- src/layout/sidebar.tsx
+++ src/layout/sidebar.tsx
@@ -1,11 +1,30 @@
-import type {Component} from "solid-js";
+import {type Component, createEffect, createSignal} from "solid-js";
+import {getHistoryConversationIdApi} from "../api/api.js";
+import {useEventSource} from "../utils/use-event-source.js";
+import {useMessagesStore} from "../stores/chat-message-store.js";
 
 const sidebar: Component = () => {
+    const {data} = useEventSource<Array<string>>(getHistoryConversationIdApi());
+
+    const [conversationIdList, setConversationId] = createSignal<Array<string>>([]);
+    createEffect(() => {
+        let d = data()
+        if (d) {
+            setConversationId([...d])
+        }
+    })
+
+    function getHistoryMessage (conversationId: string): void {
+        const {removeAllMessage,getMessage} = useMessagesStore()
+        removeAllMessage()
+        getMessage(conversationId);
+    }
+
     return (
         <ul class="menu bg-base-200 rounded-box w-56">
-            <li><a>问题 1</a></li>
-            <li><a>问题 2</a></li>
-            <li><a>问题 3</a></li>
+            {conversationIdList().map((text:string) =>{
+                return<li onClick={() => getHistoryMessage(text)}><a>{text}</a></li>
+            })}
         </ul>
     )
 }

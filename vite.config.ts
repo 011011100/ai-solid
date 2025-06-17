bfff0617dd3b7bc9253e5d96c0161043fb4867d1
LZH <LZH01101110@163.com>
2025-06-17 18:00:24 +0800

    feat:添加icons配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 import tailwindcss from "@tailwindcss/vite";
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
     plugins: [
         solid(),
         tailwindcss(),
+        // 自动引入图标组件
+        AutoImport({
+            resolvers: [IconsResolver({ prefix: 'Icon' })],
+            dts: './auto-imports.d.ts',
+        }),
+        Icons({
+            compiler: 'solid', // 使用 solid 编译器
+            autoInstall: true, // 自动安装缺少的图标库
+        }),
     ],
 })

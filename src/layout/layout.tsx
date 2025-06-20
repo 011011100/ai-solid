63d4b852646840c6a4f335f677280cb416be0036
LZH <LZH01101110@163.com>
2025-06-20 17:43:47 +0800

    feat:调整样式

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -14,7 +14,7 @@ const layout: Component<LayoutProps> = (props: LayoutProps) => {
             {/*</div>*/}
             <div class="card rounded-box place-items-center">
                 <div class="flex w-full">
-                    <div class="card rounded-box max-w-min shadow-sm grow place-items-center">
+                    <div class="card rounded-box max-w-min bg-base-200 shadow-sm grow place-items-center">
                         <Sidebar/>
                     </div>
                         {/*<div class="card w-full rounded-box shadow-sm grow place-items-center flex overflow-auto h-[calc(100vh-80px)]">*/}

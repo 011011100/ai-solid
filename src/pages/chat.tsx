fc1308313bf7d3c803303bb50c7f79b8c226ba1f
LZH <LZH01101110@163.com>
2025-06-20 17:41:02 +0800

    feat:添加主页动画样式与排版

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -10,13 +10,22 @@ const Chat = () => {
         <div class='w-100% min-w-4xl max-w-4xl max-h-[100%]'>
             {messagesStorm.messages.length == 0 && !chatStore.inside() ?
                 <div class='h-dvh flex justify-center items-center flex-col'>
-                    <p class='text-center text-4xl pb-4 '>man! what can i say?</p>
+                    <p class={`absolute mb-40 text-center text-4xl pb-4 animate-in fade-in slide-in-from-top duration-300 text-[#353535ff] ${
+                        chatStore.question() ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-full"
+                    }`}>
+                        man😉 what can i say?
+                    </p>
+                    <p class={`absolute mb-40 text-center text-4xl pb-4 animate-in fade-in slide-in-from-bottom duration-300 text-[#353535ff] ${
+                        chatStore.question() ? "opacity-0 translate-x-full" : "opacity-100 translate-x-0"
+                    }`}>
+                        say my angel!😇 say for me!
+                    </p>
                     <div>
                         <QuestionBox/>
                     </div>
                 </div> :
                 <>
-                    <div class='pb-40'>
+                    <div class='pb-40 pt-4'>
                         <HistoryChatBubble/>
                         {chatStore.inside() ?
                             <div class="daisy-text">思考中...</div> :

5a720631ac39613d818b3a4965d457a497af7ce7
LZH <LZH01101110@163.com>
2025-06-20 17:42:07 +0800

    feat:添加标题生成api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8080/'
+const BASE_URL = 'http://localhost:8080/chat/'
 
 const askQuestionApi = ((question: string , conversationId: string): string => {
     return `${BASE_URL}askQuestion?question=${question}&conversationId=${conversationId}`;
@@ -13,8 +13,13 @@ const getHistoryConversationApi = (): string => {
     return `${BASE_URL}getHistoryConversation`;
 }
 
+const createTitleApi = ((question: string , conversationId: string): string => {
+    return `${BASE_URL}createTitle?question=${question}&conversationId=${conversationId}`;
+})
+
 export {
     askQuestionApi,
     getHistoryMessageApi,
-    getHistoryConversationApi
+    getHistoryConversationApi,
+    createTitleApi,
 };

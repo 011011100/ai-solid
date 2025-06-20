c897f88cf84e8ba8bd00a4dd719b78f0d150e30b
LZH <LZH01101110@163.com>
2025-06-20 17:43:35 +0800

    feat:调整问题框样式

--- src/components/question-box/question-box.tsx
+++ src/components/question-box/question-box.tsx
@@ -5,21 +5,24 @@ const {question, addQuestion, askQuestion, inside} = useChatQuestionStore();
 
 const QuestionBox: Component = () => {
     return (
-        <div class='w-full flexitems-center flex flex-col shadow-sm p-2 rounded-md m-4 bg-white border-1 border-[#e5e5e5] border-solid'>
-            <textarea class="text-lg textarea textarea-ghost w-4xl min-h-min resize-none focus:outline-hidden p-1"
-                      rows={1}
-                      placeholder="询问任何问题"
-                      value={question()}
-                      onInput={e => addQuestion(e.target.value)}/>
-            <span class="shadow-sm p-1 badge badge-soft badge-primary badge-lg rounded-sm m-1">
-                {
-                    inside() ?
-                        <span class="loading loading-dots loading-sm"></span> :
-                        !question() ?
-                            <div>o.O</div> :
-                            <button onClick={askQuestion}>发送</button>
-                }
-            </span>
+        <div class='w-full flexitems-center flex flex-col shadow-lg p-2 rounded-md m-4 bg-base-200 border-solid'>
+            <textarea
+                class="text-lg textarea textarea-ghost w-2xl min-h-min resize-none focus:outline-hidden p-1 focus:bg-base-200"
+                rows={1}
+                placeholder="询问任何问题"
+                value={question()}
+                onInput={e => addQuestion(e.target.value)}/>
+            <div class={`transition-transform duration-300 w-min ${inside() || question() ? "translate-x-[650px]" : "translate-x-0]"}`}>
+                <span class="shadow-md p-1 badge badge-soft badge-lg rounded-lg m-1 border-[#bdbdbd]">
+                    {
+                        inside() ?
+                            <span class="loading loading-dots loading-sm"></span> :
+                            !question() ?
+                                <IconMdiLockQuestion/> :
+                                <IconMdiSend onClick={askQuestion}/>
+                    }
+                </span>
+            </div>
         </div>
     )
 }

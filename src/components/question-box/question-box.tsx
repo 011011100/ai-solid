94891c631e64bc751dda5f836263c9c251881871
LZH <LZH01101110@163.com>
2025-06-23 17:28:14 +0800

    feat:优化

--- src/components/question-box/question-box.tsx
+++ src/components/question-box/question-box.tsx
@@ -1,7 +1,7 @@
 import {useChatQuestionStore} from "../../stores/chat-question-store.js";
 import {type Component, createSignal} from "solid-js";
 
-const {question, addQuestion, askQuestion, inside} = useChatQuestionStore();
+const chatQuestionStore = useChatQuestionStore();
 
 const QuestionBox: Component = () => {
     let textareaRef: HTMLTextAreaElement | undefined;
@@ -10,10 +10,10 @@ const QuestionBox: Component = () => {
 
     const handleInput = (e: Event) => {
         const target = e.target as HTMLTextAreaElement;
-        addQuestion(target.value)
+        chatQuestionStore.addQuestion(target.value)
         let cols = Math.floor((textareaRef!.clientWidth) / parseFloat(getComputedStyle(textareaRef!).fontSize));
         // 计算行数（按换行符 +1）
-        let lines = target.value.split("\n").length - 1;
+        let lines = target.value.split("\n").length;
         target.value.split('\n').forEach((value) => {
             if (value.length > cols) {
                 lines += Math.ceil(value.length / cols)
@@ -21,6 +21,12 @@ const QuestionBox: Component = () => {
         })
         setRows(Math.min(4, Math.max(1, lines)));
     };
+
+    function handleAskQuestion() {
+        if (!chatQuestionStore.inside() && chatQuestionStore.question()) {
+            chatQuestionStore.askQuestion();
+        }
+    }
 
 
     return (
@@ -30,17 +36,19 @@ const QuestionBox: Component = () => {
                 class="text-lg textarea textarea-ghost w-2xl min-h-min resize-none focus:outline-hidden p-1 focus:bg-base-200"
                 rows={rows()}
                 placeholder="询问任何问题"
-                value={question()}
+                value={chatQuestionStore.question()}
                 onInput={e => handleInput(e)}/>
             <div
-                class={`transition-transform duration-300 w-min ${inside() || question() ? "translate-x-[650px]" : "translate-x-0]"}`}>
+                class={`transition-transform duration-300 w-min ${chatQuestionStore.inside() || chatQuestionStore.question() ? "translate-x-[650px]" : "translate-x-0]"}`}
+                onClick={handleAskQuestion}
+            >
                 <span class="shadow-md p-1 badge badge-soft badge-lg rounded-lg m-1 border-[#bdbdbd]">
                     {
-                        inside() ?
+                        chatQuestionStore.inside() ?
                             <span class="loading loading-dots loading-sm"></span> :
-                            !question() ?
+                            !chatQuestionStore.question() ?
                                 <IconMdiLockQuestion/> :
-                                <IconMdiSend onClick={askQuestion}/>
+                                <IconMdiSend/>
                     }
                 </span>
             </div>

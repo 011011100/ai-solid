1680aab69511d780b459efafd65ef1e0bc2d505c
LZH <LZH01101110@163.com>
2025-06-23 17:25:52 +0800

    feat:添加模型选择全局参数

---
+++ src/stores/chat-model-store.ts
@@ -0,0 +1,47 @@
+import {createEffect, createRoot, createSignal} from "solid-js";
+import {useEventSource} from "../utils/use-event-source.js";
+import {getAllChatModel} from "../api/ollama-api.js";
+// import {type OllamaChatModelTag, OllamaChatModelTagsResponse} from "../type/ollama-chat-model-tag.js";
+import {DeepseekChatModelTag, type DeepseekChatModelTagsResponse} from "../type/deepseek-chat-model-tag.js";
+
+let store: ReturnType<typeof createChatModelStore>;
+
+function createChatModelStore() {
+    const [modelNameList, setModelNameList] = createSignal<string[]>([]);
+    // const [modelList, setModelList] = createSignal<OllamaChatModelTag[]>([]);
+    const [modelList, setModelList] = createSignal<DeepseekChatModelTag[]>([]);
+
+    function getAllModelName() {
+        setModelNameList([]);
+        // let {data} = useEventSource<OllamaChatModelTagsResponse>(getAllChatModel())
+        let {data} = useEventSource<DeepseekChatModelTagsResponse>(getAllChatModel())
+        createRoot(() => {
+            createEffect(() => {
+                let d = data()?.data;
+                if (d) {
+                    setModelList([...d]);
+                    setModelNameList(d.map((item) => item.id));
+                }
+            })
+        })
+    }
+
+    const [model, setModel] = createSignal<string>('');
+
+    function setModelName(name: string) {
+        setModel(name);
+    }
+
+    return {
+        modelList,
+        modelNameList,
+        getAllModelName,
+        model,
+        setModelName,
+    };
+}
+
+export function useChatModelStore() {
+    if (!store) store = createChatModelStore();
+    return store;
+}

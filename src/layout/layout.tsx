5c3033305ca3e60847c87f2c793aef30df2ecf0e
LZH <LZH01101110@163.com>
2025-06-23 17:30:30 +0800

    feat:添加侧栏收缩标签

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,6 +1,6 @@
-// import Header from "./header.js";
 import Sidebar from "./sidebar.js";
-import type {Component, JSX} from "solid-js";
+import {type Component, type JSX} from "solid-js";
+import ChooseModel from "../components/choose-model/choose-model.js";
 
 type LayoutProps = {
     children: JSX.Element;
@@ -17,9 +17,19 @@ const layout: Component<LayoutProps> = (props: LayoutProps) => {
                     <div class="card rounded-box max-w-min bg-base-200 shadow-sm grow place-items-center">
                         <Sidebar/>
                     </div>
-                        {/*<div class="card w-full rounded-box shadow-sm grow place-items-center flex overflow-auto h-[calc(100vh-80px)]">*/}
-                        <div class="card w-full rounded-box shadow-sm grow place-items-center flex overflow-auto h-[calc(100vh)]">
-                        {props.children}
+                    {/*<div class="card w-full rounded-box shadow-sm grow place-items-center flex overflow-auto h-[calc(100vh-80px)]">*/}
+                    <div class='card w-full'>
+                        <div
+                            class='absolute top-0 w-full h-20 bg-transparent text-right p-3 flex items-start justify-between'>
+                            <button>
+                                <IconMdiMenuOpen class='size-6'/>
+                            </button>
+                            <ChooseModel/>
+                        </div>
+                        <div
+                            class="card w-full rounded-box shadow-sm grow place-items-center flex overflow-auto h-[calc(100vh)]">
+                            {props.children}
+                        </div>
                     </div>
                 </div>
             </div>

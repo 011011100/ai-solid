e1124a9f7be65e8812271930a8397d6aff666391
li <140138370@qq.com>
2025-06-24 23:12:46 +0800

    feat: 调整渲染方式

--- src/components/choose-model/choose-model.tsx
+++ src/components/choose-model/choose-model.tsx
@@ -1,4 +1,4 @@
-import {onMount, type Component} from "solid-js";
+import {onMount, type Component, Show} from "solid-js";
 import {useChatModelStore} from "../../stores/chat-model-store.js";
 
 const chooseModel: Component = () => {
@@ -13,12 +13,14 @@ const chooseModel: Component = () => {
         <div class="dropdown dropdown-end z-10">
             <p class="label mr-1">选择模型</p>
             <div tabIndex={0} role="button" class="m-1 text-lg ">
-                {chatModelStore.model() || chatModelStore.modelNameList()?.length !== 0 ? "选择模型" : "什么都没有"}
+                <Show when={chatModelStore.model()} fallback={"选择模型"}>
+                    {chatModelStore.model()}
+                </Show>
             </div>
             {chatModelStore.modelNameList()?.length !== 0 ?
                 <ul tabIndex={0}
                     class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-                    {chatModelStore.modelNameList()!.map(name => {
+                    {chatModelStore.modelNameList()?.map(name => {
                         return <li>
                             <a onClick={() => chatModelStore.setModelName(name)}>
                                 {name}

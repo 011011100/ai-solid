69e4f8bbb263e3b61f784c30ab2f8fd5585ed374
li <140138370@qq.com>
2025-06-28 23:37:19 +0800

    feat: 添加新记忆栏

--- src/components/menu/function-menu.tsx
+++ src/components/menu/function-menu.tsx
@@ -2,7 +2,7 @@ import type {Component} from "solid-js";
 import {useMessagesStore} from "../../stores/chat-message-store.js";
 import {useChatQuestionStore} from "../../stores/chat-question-store.js";
 
-const newQuestionMenu: Component = () => {
+const FunctionMenu: Component = () => {
 
     const {removeAllMessage} = useMessagesStore()
 
@@ -19,12 +19,16 @@ const newQuestionMenu: Component = () => {
         <ul class="menu bg-base-200 rounded-box w-56">
             <li onClick={() => newQuestion()}>
                 <a class='flex items-center'>
-                    <IconMdiFileDocumentPlusOutline class="w-4 h-4"/>
+                    <IconLineMdFileDocumentPlus class="w-4 h-4"/>
                     <p class='text-base'>新问题</p>
+                </a>
+                <a class='flex items-center'>
+                    <IconLineMdFileUpload class="w-4 h-4"/>
+                    <p class='text-base'>新记忆</p>
                 </a>
             </li>
         </ul>
     )
 }
 
-export default newQuestionMenu
+export default FunctionMenu

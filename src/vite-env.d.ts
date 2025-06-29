673a35f22eb1a8480071aa4977bcd32451822cd0
li <140138370@qq.com>
2025-06-29 13:25:20 +0800

    feat: 调整配置

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+import "solid-js";
+
+declare module "solid-js" {
+    namespace JSX {
+        interface Directives {
+            autoFocus: boolean;
+        }
+    }
+}

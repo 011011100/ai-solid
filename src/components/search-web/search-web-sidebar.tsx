4f666fbabf2fc44e097e02bd6f6470a033d09cb7
li <140138370@qq.com>
2025-07-04 21:39:54 +0800

    feat: 设置联网查询内容展示

--- src/components/search-web/search-web-sidebar.tsx
+++ src/components/search-web/search-web-sidebar.tsx
@@ -1,4 +1,4 @@
-import { Transition } from "solid-transition-group";
+import {Transition} from "solid-transition-group";
 import {useChatOnlineStore} from "../../stores/chat-online-store.js";
 import "./search-web-sidebar.css"
 
@@ -9,17 +9,38 @@ const searchWebSidebar = () => {
     return (
         <div class="relative">
             <Transition name="slide-right">
-                {chatOnlineStore.webPageInfo() && (
+                {chatOnlineStore.openSidebar() && (
                     <div
-                        class="fixed top-0 right-0 h-full bg-white shadow-lg p-4 transition-all duration-300 ease-in-out z-50"
-                        style={{ width: "300px" }}
+                        class="fixed top-0 right-0 h-full bg-white shadow-lg p-4 transition-all duration-300 ease-in-out z-50 h-100vh overflow-y-auto"
+                        style={{width: "300px"}}
                     >
                         <h2 class="text-lg font-bold mb-2">联网搜索结果</h2>
                         <ul class="space-y-1">
                             {chatOnlineStore?.webPageInfo()?.map((item) => (
-                                <li class="text-sm text-gray-800">
-                                    {item.id}
-                                </li>
+                                <>
+                                    <div class="divider"/>
+                                    <li class="text-sm text-gray-800">
+                                        <div>
+                                            <div class='flex flex-nowrap items-center'>
+                                                <img src={item.icon} alt={item.web_anchor}
+                                                     class='rounded-lg size-5 b-solid border border-gray-200 border-solid mr-2'/>
+                                                {item.web_anchor}
+                                            </div>
+                                            <a href={item.url} target="_blank" rel="noopener noreferrer">
+                                                <div class='font-bold text-base mt-1 mb-1 hover:text-blue-400'>
+                                                    {item.title}
+                                                </div>
+                                            </a>
+
+                                        </div>
+                                        <div>
+                                            {item.content}
+                                        </div>
+                                        <p class="text-sm text-gray-400">
+                                            {item.date}
+                                        </p>
+                                    </li>
+                                </>
                             ))}
                         </ul>
                     </div>
